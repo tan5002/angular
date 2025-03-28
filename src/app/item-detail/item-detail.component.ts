d360c9a051981dc76259e03f750647b58976b60e
tan5002 <ntan15761@gmail.com>
2025-03-28 18:08:46 +0700

    update

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Item from '../item/Item';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,6 +9,28 @@ import { Component, input } from '@angular/core';
   styleUrl: './item-detail.component.css'
 })
 export class ItemDetailComponent {
-  id = input.required<number>();
-  name = input.required<string>();
+  items : Item[] = [
+    {id: 1, name: "John", description: "abc",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 2, name: "Jane", description: "def",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 3, name: "Jim", description: "ghi",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 4, name: "Jill", description: "jkl",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 5, name: "Jack", description: "mno",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 6, name: "Jill", description: "pqr",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 7, name: "Jane", description: "stu",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 8, name: "Jim", description: "vwx",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 9, name: "Jack", description: "yza",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20},
+    {id: 10, name: "John", description: "bcd",image: "https://plus.unsplash.com/premium_photo-1742945845688-d2e666a3b92a?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",price: 20}
+  ];
+  item: any;
+  itemId!: number;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Lấy id từ URL
+    this.itemId = Number(this.route.snapshot.paramMap.get('id'));
+
+    // Tìm item theo id
+    this.item = this.items.find(i => i.id === this.itemId);
+  }
 }
--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-form',
   standalone: false,
   templateUrl: './profile-form.component.html',
-  styleUrl: './profile-form.component.css'
+  styleUrls: ['./profile-form.component.css']
 })
 export class ProfileFormComponent {
+  profileForm = new FormGroup({
+    basicInfo: new FormGroup({
+      firstName: new FormControl("", Validators.required), 
+      lastName: new FormControl("", Validators.required),
+      email: new FormControl("", [Validators.email, Validators.required]),
+      phone: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+    }),
+    educationInfo: new FormGroup({ 
+      elementarySchool: new FormControl(""),
+      secondarySchool: new FormControl(""),
+      highSchool: new FormControl(""),
+    }),
+  });
 
+  get phone() {
+    return this.profileForm.get("basicInfo")?.get("phone");
+  }
+
+  hasPhoneMinLengthError() {
+    return this.phone?.errors?.["minlength"];
+  }
+
+  hasPhoneMaxLengthError() {
+    return this.phone?.errors?.["maxlength"];
+  }
+
+  hasPhoneRequiredError() {
+    return this.phone?.errors?.["required"];
+  }
 }
